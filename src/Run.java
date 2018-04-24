136f20820fdd7e177ced2888b90464ad1717db04
Fernando Isaías García Aguirre <ferisagaragu@gmail.com>
2018-04-24 16:32:48 -0500

    I add new methods to Json class

--- src/Run.java
+++ src/Run.java
@@ -1,19 +1,14 @@
 import org.javabrain.util.alert.Console;
 import org.javabrain.util.data.Json;
 import org.javabrain.util.resource.Path;
-import org.javabrain.util.web.service.Petition;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import java.util.*;
 
 public class Run {
     public static void main(String[] args) throws ParseException {
 
         Json json = new Json(Path.get("org.javabrain.util.data.test","json"));
-        Console.blue(json.getJSONArray("array").exclude(0));
+        Console.blue(json.getDate("item3","dd.MM.yy HH:MM"));
     }
 
     /*todo Classes por añadir
