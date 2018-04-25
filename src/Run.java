8a308443c724a3ef07e28abf3ceab6cbe0c6232d
Fernando Isaías García Aguirre <ferisagaragu@gmail.com>
2018-04-24 23:48:05 -0500

    I add more methods to Json class for to scalate to 0.0.3 version

--- src/Run.java
+++ src/Run.java
@@ -3,13 +3,24 @@ import org.javabrain.util.data.Json;
 import org.javabrain.util.resource.Path;
 import org.json.simple.parser.ParseException;
 
-import java.util.*;
 
 public class Run {
+
     public static void main(String[] args) throws ParseException {
 
         Json json = new Json(Path.get("org.javabrain.util.data.test","json"));
-        Console.blue(json.getJSONArray("array").exclude(0));
+        Json json1 = new Json();
+        Json out = new Json();
+        json1.put("marrufo","alexa");
+        json1.put("alexa","no manches");
+
+
+
+        //json1.putJSONArray("arra2",json1);
+
+        out.putJSONArray("array2", json1);
+        Console.red(out.toMap());
+        Console.red(json.putJSONInJSONArray("array",out)); //esta bien por que este no es el elemento al que se debe llamar si no al putObjectInJSONArray
     }
 
     /*todo Classes por añadir
