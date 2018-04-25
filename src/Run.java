a10a5166741e43826cd64faecab25cf58ad1f0da
Fernando Isaías García Aguirre <ferisagaragu@gmail.com>
2018-04-24 21:47:04 -0500

    I integrate code

--- src/Run.java
+++ src/Run.java
@@ -3,21 +3,13 @@ import org.javabrain.util.data.Json;
 import org.javabrain.util.resource.Path;
 import org.json.simple.parser.ParseException;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Run {
     public static void main(String[] args) throws ParseException {
 
-        Json json = new Json(Path.getExternal("doc.json","json",""));
-        ArrayList<Json> jsons = json.getJSON("method").getJSON("get").values();
-
-        for(Json json1:jsons){
-            Console.green(json1.getString("name")+" -> "+json1.getJSONArray("parameter",0).getString("type"));
-        }
-
-        json.put("accion",123);
-        Console.blue(json);
-
+        Json json = new Json(Path.get("org.javabrain.util.data.test","json"));
+        Console.blue(json.getJSONArray("array").exclude(0));
     }
 
     /*todo Classes por añadir
