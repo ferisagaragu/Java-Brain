f6322265feca9fd9c1fa6d95bc9f004c23b02d1f
Fernando Isaías García Aguirre <ferisagaragu@gmail.com>
2018-11-08 10:37:30 -0600

    I add absolute path in proyect

--- library/src/org/javabrain/util/resource/Archive.java
+++ library/src/org/javabrain/util/resource/Archive.java
@@ -1,32 +1,49 @@
 package org.javabrain.util.resource;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import org.javabrain.util.alert.Log;
 
 
 /**
  * @author Fernando García
  * @version 0.0.3
+ * @note [] se usara para hacer una ruta relativa fuera de la carpeta src pero dentro de la del proyecto
  */
 public class Archive {
 
     public static final ClassLoader CLASS_LOADER = ClassLoader.getSystemClassLoader();
     
+    public static final String PROYECT_PATH = System.getProperty("user.dir");
+    public static final String SOURCE_PATH = System.getProperty("user.dir") + "\\src\\";
+    
     public static String road(String path) {
-
+        
+        String firsPath = "";
+        
         //Validacion de cadenas internas con ->.{} 
         if (path.contains("{")) {
             String[] sub = path.split("\\{");
-            String firsPath = sub[0].replace(".", "/");
+            firsPath = sub[0].replace(".", "/");
             String endPath = sub[1].replace("{", "").replace("}", "");
             path = firsPath + endPath;
         }
 
+        //Valida cadenas internas con [] para crear una ruta en la carpeta del proyecto
+        if (path.contains("[")) {
+            File f = new File(firsPath.replace("[", ""));
+            f.mkdirs();
+            path = PROYECT_PATH +"\\"+ path.replace("[", "").replace("]", "").replace("/","\\");
+        }
+        
         //Validacion de cadenas internas con -> / 
         switch (path.charAt(0)) {
             case '/':
@@ -43,9 +60,9 @@ public class Archive {
         String outStg;
         String str;
         outStg = "";
-        InputStream inn = CLASS_LOADER.getResourceAsStream(Archive.road(path));
+        InputStream inn = CLASS_LOADER.getResourceAsStream(road(path));
 
-        try (InputStream in = inn == null ? new FileInputStream(new File(path)) : inn) {
+        try (InputStream in = inn == null ? new FileInputStream(new File(road(path))) : inn) {
             out = new BufferedReader(new InputStreamReader(in, "UTF8"));
             while ((str = out.readLine()) != null) {
                 outStg = outStg + str + "\n";
@@ -57,5 +74,18 @@ public class Archive {
         return null;
     }
 
+    public static boolean write(String path, String text) {
+        
+        try (Writer out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(road(path)), "UTF8"))) {
+            out.append(text);
+            out.flush();
+            out.close();
+            return true;
+        } catch (IOException ioe) {
+            Log.error("Could not create or write the file, check that the route is correct or exists.",ioe);
+        }
 
+        return false;
+    }
+    
 }
